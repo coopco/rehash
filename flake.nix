4ace1b9d49d062a557325b8fa9fdde52a9dbd04e
Connor Cooper <connor.cooper@proton.me>
2025-06-16 13:06:12 +1000

    Add shell integration files to nix store

--- flake.nix
+++ flake.nix
@@ -31,6 +31,11 @@
           version = "0.1.0";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
+
+          postInstall = ''
+            mkdir -p $out/shell
+            cp shell/* $out/shell/
+          '';
         };
       });
 }
